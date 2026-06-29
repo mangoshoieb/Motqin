8aaf507a758f13d01fb7450f8f956988ea23d38c
Amgad Mango <amgadmagdy667@gmail.com>
2026-06-30 00:42:29 +0300

    some changes on facebook login

--- frontend/app/services/facebook-auth.services.ts
+++ frontend/app/services/facebook-auth.services.ts
@@ -1,31 +1,29 @@
 export const signInWithFacebook = (): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      if (!window.FB) {
-        reject(new Error("Facebook SDK not loaded"));
-        return;
-      }
-      console.log(window.FB);
-      console.log("Calling FB.login...");
-      window.FB.login(
-        (response) => {
-          console.log("FB Response:", response);
-      
-          if (
-            response.authResponse &&
-            response.authResponse.accessToken
-          ) {
-            console.log("facebook login success");
-      
-            resolve(response.authResponse.accessToken);
-          } else {
-            console.log("Login cancelled", response);
-      
-            reject(new Error("Facebook login cancelled"));
-          }
-        },
-        {
-          scope: "public_profile,email",
+  return new Promise((resolve, reject) => {
+    if (!window.FB) {
+      reject("SDK not loaded");
+      return;
+    }
+
+    console.log("Calling FB.login...");
+
+    window.FB.login(
+      (response) => {
+        console.log("FB Response:", response);
+
+        if (response.status === "connected") {
+          console.log("Access Token:", response.authResponse?.accessToken);
+
+          resolve(response.authResponse!.accessToken);
+        } else {
+          console.log("Login cancelled");
+
+          reject(response);
         }
-      );
-    });
-  };
+      },
+      {
+        scope: "public_profile,email",
+      }
+    );
+  });
+};
